f14886b7606f1ce9a33187c0b0d760d26e664d1c
ismet kizgin <info@ismetkizgin.com>
2020-07-21 15:50:05 +0300

    feat: added stream router

---
+++ routers/streamRouter.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const fs = require('fs');
+const router = express();
+
+router.get('/stream/:resolution', async (req, res) => {
+	try {
+		const resolution = req.params.resolution;
+		const path = `public/assets/videos/peru-${resolution}.mp4`;
+		const stat = fs.statSync(path);
+		const fileSize = stat.size;
+		const range = req.headers.range;
+		if (range) {
+			const parts = range.replace(/bytes=/, "").split("-");
+			const start = parseInt(parts[0], 10);
+			const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+			const chunksize = (end - start) + 1;
+			const file = fs.createReadStream(path, { start, end });
+			const head = {
+				'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+				'Accept-Ranges': 'bytes',
+				'Content-Length': chunksize,
+				'Content-Type': 'video/mp4',
+			};
+			res.writeHead(206, head);
+			file.pipe(res);
+		} else {
+			const head = {
+				'Content-Length': fileSize,
+				'Content-Type': 'video/mp4',
+			};
+			res.writeHead(200, head);
+			fs.createReadStream(path).pipe(res);
+		}
+	} catch (err) {
+		res.status(400).send(err.message);
+	}
+});
+
+module.exports = router;
--- serve.js
+++ serve.js
@@ -1,12 +1,14 @@
-
 const path = require('path');
 const express = require('express');
 const app = express();
+const routers = require('./routers');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(routers.streamRouter);
+
 app.use((req, res, next) => {
-    res.send("404 NOT FOUND");
+    res.status(404).send("404 NOT FOUND");
 })
 
 module.exports = app;
